d2213e1c8eba5839a71a15358e6b925b0f63d686
Julien KAPLAN <julien.kaplan@etu.univ-cotedazur.fr>
2022-04-18 22:44:13 +0200

    #1 add Store

--- Draf-your-Draft/src/App.vue
+++ Draf-your-Draft/src/App.vue
@@ -2,12 +2,19 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <button v-on:click="increment">test</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+  increment() {
+    this.$store.commit('increment')
+    console.log(this.$store.state.count)
+  }
+}
 }
 </script>
 
--- Draf-your-Draft/src/main.js
+++ Draf-your-Draft/src/main.js
@@ -3,13 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import store from './store/mainStore'
+import Vuex from 'vuex'
 Vue.config.productionTip = false
-
 /* eslint-disable no-new */
+
 new Vue({
   el: '#app',
   router,
+  store: store,
   components: { App },
   template: '<App/>'
 })
