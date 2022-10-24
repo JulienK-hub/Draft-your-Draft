155aa11cf6469a21cdd855cc25573b1f7a04e0aa
Julien KAPLAN <julien.kaplan@sii.fr>
2022-10-24 14:16:59 +0200

    creation label (css à revoir)  #6

--- Draf-your-Draft/src/components/LabelsPage/LabelsPage.vue
+++ Draf-your-Draft/src/components/LabelsPage/LabelsPage.vue
@@ -7,7 +7,17 @@
   <button v-on:click="goToPage('/DraftPage')">
     Draft
   </button>
-  </div>
+  <h2 class="title">Create a label</h2>
+  <p style="display:inline">Name</p>
+  <input v-model="labelName" type="text"/>
+  <p style="display:inline">Color</p>
+  <input style="display:inline" v-model="labelBGColor" type="color" id="colorpicker" value="#0000ff"/>
+  
+    <p style="display:inline">Black</p>
+    <input id="black" type="checkbox" @change="changeTextLabelColor()"/>
+
+  <p :style="{'background-color':labelBGColor,'color':labelTextColor}">{{labelName}}</p>
+</div>
 </template>
 
 <script>
@@ -15,10 +25,21 @@ export default {
   name: 'LabelsPage',
   data () {
     return {
+      labelBGColor: "#0000ff",
+      labelName: "",
+      labelTextColor: "white"
     }
   },
   
   methods: {
+    changeTextLabelColor () {
+      if(this.labelTextColor == "white") {
+        this.labelTextColor = "black";
+      }
+      else{
+        this.labelTextColor = "white";
+      }
+    },
     goToPage(pageURL){
       this.$router.push(pageURL)
     }
@@ -27,4 +48,7 @@ export default {
 </script>
 
 <style scoped>
+.title{
+  padding-right: 0%;
+}
 </style>
